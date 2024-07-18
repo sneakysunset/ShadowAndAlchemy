4f62e1487a31c09d623da767594014204a713a0d
Sneakysunset <andrelauzier@gmail.com>
2024-07-18 10:04:52 +0200

    Grabbable scripts + 2D Package

---
+++ Assets/Scripts/Items/IGrabbable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public interface IGrabbable
+{
+    public void Grab(Transform parent);
+
+    public void Release();
+
+    public bool IsHeld { get; }
+
+    public bool IsHoldable();
+
+    public bool Throw(Vector2 endPosition);
+}
